e327308ab2dfb81367039642743b297a5e7fbd90
Kowsar Ahmed <kahmed.dev@gmail.com>
2018-10-18 22:01:12 -0400

    Fixing binary search conditions

--- algorithms/search/binary-search.cpp
+++ algorithms/search/binary-search.cpp
@@ -8,9 +8,9 @@ int binarySearch(vector<int>& numbers, int item) {
   int high = numbers.size() - 1;
   while (low <= high) {
     int mid = (low + high) / 2;
-    if (numbers[mid] > item) {
+    if (item > numbers[mid]) {
       low = mid + 1;
-    } else if (numbers[mid] < item) {
+    } else if (item < numbers[mid]) {
       high = mid - 1;
     } else {
       return mid;
@@ -22,7 +22,7 @@ int binarySearch(vector<int>& numbers, int item) {
 
 int main() {
   vector<int> numbers = {1, 2, 4, 5, 6, 7, 8, 12, 48};
-  cout << binarySearch(numbers, 6) << endl;
+  cout << binarySearch(numbers, 12) << endl;
   cout << binarySearch(numbers, 9) << endl;
   vector<int> empty;
   cout << binarySearch(empty, 6) << endl;
