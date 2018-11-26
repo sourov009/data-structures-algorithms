262bea7fdba1c9c97a778e28726fb098348877a3
Kowsar Ahmed <kahmed.dev@gmail.com>
2018-11-26 00:40:40 -0500

    Minor optimization

--- algorithms/sort/selection-sort.cpp
+++ algorithms/sort/selection-sort.cpp
@@ -8,7 +8,7 @@ void print(vector<int>& numbers);
 void selectionSort(vector<int>& numbers);
 
 int main() {
-  vector<int> numbers = {28, 24, 1, 4, 20, 4, 99, 10, 212};
+  vector<int> numbers = {28, 24, 1, 4, 20, 4, 99, 10, 212, 7};
   print(numbers);
   selectionSort(numbers);
   print(numbers);
@@ -28,9 +28,10 @@ void print(vector<int>& numbers) {
 }
 
 void selectionSort(vector<int>& numbers) {
-  for (int i = 0; i < numbers.size()-1; ++i) {
+  int n = numbers.size();
+  for (int i = 0; i < n-1; ++i) {
     int minIndex = i;
-    for (int j = i+1; j < numbers.size(); ++j) {
+    for (int j = i+1; j < n; ++j) {
       if (numbers[j] < numbers[minIndex]) {
         minIndex = j;
       }
