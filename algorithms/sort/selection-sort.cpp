0b29fd652a1af1601b19e8a72607fb4d30d60949
Kowsar Ahmed <kahmed.dev@gmail.com>
2018-11-26 00:20:54 -0500

    Fixing upper bound of outer loop

--- algorithms/sort/selection-sort.cpp
+++ algorithms/sort/selection-sort.cpp
@@ -28,7 +28,7 @@ void print(vector<int>& numbers) {
 }
 
 void selectionSort(vector<int>& numbers) {
-  for (int i = 0; i < numbers.size(); ++i) {
+  for (int i = 0; i < numbers.size()-1; ++i) {
     int minIndex = i;
     for (int j = i+1; j < numbers.size(); ++j) {
       if (numbers[j] < numbers[minIndex]) {
